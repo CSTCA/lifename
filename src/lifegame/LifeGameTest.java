06bf975da7ac7ac259b67d04969369387ce5837f
CSTCA <1943847138@qq.com>
2019-06-11 19:14:28 +0800

    生命游戏

---
+++ src/lifegame/LifeGameTest.java
@@ -0,0 +1,29 @@
+package lifegame;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LifeGameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLifeGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
